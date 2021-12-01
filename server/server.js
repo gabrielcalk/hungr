85405168da32b230639d4664aea6aa1a32011014
Fenix Sampson <Fenix.sampson@gmail.com>
2021-12-01 13:52:21 -0500

    updated package.json with needed dependencies

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 require('dotenv').config();
 const compression = require('compression')
+const { Sequelize, Model, DataTypes } = require('sequelize');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +14,6 @@ if (process.env.NODE_ENV === 'production') {
   }
   
 
-  sequelize.sync({ force: false }).then(() => {
+  Sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on PORT: ${PORT}: http://localhost:${PORT}`))
 });
