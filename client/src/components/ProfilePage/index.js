d5cd1e9c4c6a3c4e707347c5689eb623cb0e7c3f
Fenix Sampson <Fenix.sampson@gmail.com>
2021-12-02 17:43:57 -0500

    building profile account info component

--- client/src/components/ProfilePage/index.js
+++ client/src/components/ProfilePage/index.js
@@ -1,9 +1,13 @@
 import RenderProfileUser from '../ProfileUser/index'
+import RenderProfileInfo from '../ProfileInfo';
 
 export default function RenderProfile() {
     return (
         <>
             <RenderProfileUser/>   
+
+            
+            <RenderProfileInfo/>   
         </>
     );
 }
