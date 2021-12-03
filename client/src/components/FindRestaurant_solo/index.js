a91bab601e2ce7c000d5cd3647114900a1034666
Brent Graves <graves.brent@gmail.com>
2021-12-03 14:49:12 -0500

    started restaurant questionaire conversion.

---
+++ client/src/components/FindRestaurant_solo/index.js
@@ -0,0 +1,39 @@
+import './style.css'
+
+import RenderFindRestaurant from '../FindRestaurant'
+
+
+
+export default function RendersoloRestaurant(){
+    return (
+            <section>   
+                <section>
+                    <h4>Find Restaurant - Indulge Alone!</h4>
+                </section>
+                    <section className="find_restaurant_solo_card_body">
+                        <form>
+                            <p>Where would you like to look for restaurants?</p>
+                            <input className="form-input" placeholder="Location" name="location" type="location" />
+                            <br></br>
+
+                            <p>What type of cuisine would you like to eat?</p>
+                            <input className="" placeholder="Cuisine" name="cuisine" type="text" />
+                            <br></br>
+
+                            <p>What would you like to spend?</p>
+                            <input className="form-input" placeholder="Meal Budget" name="budget" type="text" />
+                            <br></br>
+
+                           
+                            <p>Minimum Restaurant Rating?</p>
+                            <input className="form-input" placeholder="Select Rating" name="rating" type="text" />
+                            <br></br>
+
+                            <button className="find_restaurant_button" type="submit">
+                            Find Restaurant
+                            </button>
+                        </form>
+                    </section>
+        </section>
+    );
+}
