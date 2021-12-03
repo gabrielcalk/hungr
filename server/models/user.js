62d11887fbb090475ff6ddd083d2abce95f7d9f3
faithelizagreen <faithgreen@knights.ucf.edu>
2021-12-02 20:43:29 -0600

    fixed issues with seeds

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt")
 const sequelize = require("../config/connections");
+
 
 // sequelize user model
 class User extends Model {}
