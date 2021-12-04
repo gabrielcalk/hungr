cece26b8c0049cee004120b28151b09156c62143
faithelizagreen <faithgreen@knights.ucf.edu>
2021-12-04 16:36:15 -0600

    trying to  render restaurant search forms -- not ready for merge

--- client/src/components/FindRestaurant/index.js
+++ client/src/components/FindRestaurant/index.js
@@ -1,6 +1,12 @@
-import './style.css'
+import './style.css';
+import RenderFindRestaurantSolo from '../FindRestaurant_solo'
+import RenderFindRestaurantFriend from '../FindRestaurant_friend'
 
-export default function RenderFindRestaurant() {
+
+export default function RenderFindRestaurant({setRestaurantComponent}) {
+ 
+
+
     return (
         <section className="component_right_restaurant">
             <div className="div_title_component_right">
@@ -8,13 +14,15 @@ export default function RenderFindRestaurant() {
             </div>
             <section className="options_restaurant">
                 <h1>Invite A Friend</h1>
-                <button className="button_invite">Restaurant Request</button>
+                <button onClick={() => setRestaurantComponent('find_restaurant_friend')} className="button_invite">Restaurant Request</button>
 
                 <h1 className="or_invite">----- Or -----</h1>
 
                 <h1>Indulge Alone</h1>
-                <button className="button_invite">Find a Restaurant</button>
+                <button onClick={() => setRestaurantComponent('find_restraurant_solo')} className="button_invite">Find a Restaurant</button>
             </section>
+            <RenderFindRestaurantSolo/>
+            <RenderFindRestaurantFriend/>
         </section>
     );
 }
--- client/src/components/ProfilePage/index.js
+++ client/src/components/ProfilePage/index.js
@@ -3,6 +3,8 @@ import RenderNavSignIn from "../NavLogin/index";
 import RenderProfileComponent from '../ProfileUser/index'
 import RenderProfileInfo from '../ProfileInfo';
 import RenderFindRestaurant from '../FindRestaurant/index'
+import RenderFindRestaurantSolo from '../FindRestaurant_solo'
+import RenderFindRestaurantFriend from '../FindRestaurant_friend'
 import { QUERY_ME } from '../../utils/queries';
 import {useState} from 'react'
 import './style.css'
@@ -13,12 +15,17 @@ export default function RenderProfile() {
     const { loading, data } = useQuery(QUERY_ME);
     const userData = data?.me || {};
     const [profileComponent, setProfileComponent] = useState('find_restaurant')
+    const [restaurantComponent, setRestaurantComponent] = useState('find_restaurant_friend')
     // component that could show the friend, invite and add new friend component
     function renderComponentOnRight(){
         if(profileComponent === 'find_restaurant'){
             return <RenderFindRestaurant/>
         } else if(profileComponent === 'profile_info'){
             return <RenderProfileInfo/>
+        } else if (restaurantComponent === 'find_restaurant_friend') {
+            return <RenderFindRestaurantFriend/>
+        } else if (restaurantComponent === 'find_restaurant_solo') {
+            return <RenderFindRestaurantSolo/>
         }
     }
 
@@ -37,6 +44,7 @@ export default function RenderProfile() {
             <main className="main_profile">
                 <RenderProfileComponent username={userData.username} email={userData.email} setProfileComponent={setProfileComponent}/>
                 {renderComponentOnRight()}   
+                <RenderFindRestaurant setRestaurantComponent={setRestaurantComponent}/>
             </main>
         </>
     );
